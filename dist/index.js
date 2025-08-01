852bf15449f0b951b21c27e24c21e432b9678f9a
Randell <piszmogcode@gmail.com>
2025-07-31 22:18:56 -0600

    Change to CJS

--- dist/index.js
+++ dist/index.js
@@ -29189,46 +29189,39 @@ function wrappy (fn, cb) {
 /***/ }),
 
 /***/ 1713:
-/***/ ((module, __webpack_exports__, __nccwpck_require__) => {
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-"use strict";
-__nccwpck_require__.r(__webpack_exports__);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(2186);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__nccwpck_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(5438);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__nccwpck_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_1__);
-/* module decorator */ module = __nccwpck_require__.hmd(module);
-
-
+const core = __nccwpck_require__(2186);
+const github = __nccwpck_require__(5438);
 
 const run = async () => {
-  const tag = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('version')
+  const tag = core.getInput('version')
 
-  const client = _actions_github__WEBPACK_IMPORTED_MODULE_1__.getOctokit(_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('token'))
+  const client = github.getOctokit(core.getInput('token'))
 
   const tag_rsp = await client.rest.git.createTag({
-    ..._actions_github__WEBPACK_IMPORTED_MODULE_1__.context.repo,
+    ...github.context.repo,
     tag,
-    message: _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('message'),
-    object: _actions_github__WEBPACK_IMPORTED_MODULE_1__.context.sha,
+    message: core.getInput('message'),
+    object: github.context.sha,
     type: 'commit'
   })
   if (tag_rsp.status !== 201) {
-    _actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(`Failed to create tag object (status=${tag_rsp.status})`)
+    core.setFailed(`Failed to create tag object (status=${tag_rsp.status})`)
     return
   }
 
   const ref_rsp = await client.rest.git.createRef({
-    ..._actions_github__WEBPACK_IMPORTED_MODULE_1__.context.repo,
+    ...github.context.repo,
     ref: `refs/tags/${tag}`,
     sha: tag_rsp.data.sha
   })
   if (ref_rsp.status !== 201) {
-    _actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(`Failed to create tag ref(status = ${tag_rsp.status})`)
+    core.setFailed(`Failed to create tag ref(status = ${tag_rsp.status})`)
     return
   }
 
-  _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`Tagged ${tag_rsp.data.sha} as ${tag}`)
+  core.info(`Tagged ${tag_rsp.data.sha} as ${tag}`)
 };
 
 run();
@@ -31101,8 +31094,8 @@ module.exports = parseParams
 /******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			id: moduleId,
-/******/ 			loaded: false,
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
 /******/ 	
@@ -31115,69 +31108,11 @@ module.exports = parseParams
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
 /******/ 		}
 /******/ 	
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-/******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__nccwpck_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__nccwpck_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/harmony module decorator */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.hmd = (module) => {
-/******/ 			module = Object.create(module);
-/******/ 			if (!module.children) module.children = [];
-/******/ 			Object.defineProperty(module, 'exports', {
-/******/ 				enumerable: true,
-/******/ 				set: () => {
-/******/ 					throw new Error('ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ' + module.id);
-/******/ 				}
-/******/ 			});
-/******/ 			return module;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
